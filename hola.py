0f696e5b9d6988eb1df3155c54a988b4154e01c9
osm3rr <osm3rr@adakademy.com>
2026-02-26 19:31:06 -0400

    estructuras de datos y operadores

--- hola.py
+++ hola.py
@@ -1,1 +1,138 @@
-print("Hola mundo")
+# Variables en python
+name = "Ruperto" # string
+#age = 30 # integer
+height = 1.75 # float   
+is_student = True # boolean
+
+
+# malas practicas
+x = 10
+Name = "Juan"
+#name-last_name = "Perez"
+#number.first = 5
+AGE = 25
+PI = 3.14
+
+# operadores matematicos basicos
+number1 = 10
+number2 = 5
+
+# suma
+result_sum = number1 + number2
+# print(result_sum)
+
+# print("El resultado de la suma es:", result_sum)
+
+# print(f"El resultado de la suma es: {result_sum}")
+
+# resta
+result_subtraction = number1 - number2
+# print(f"El resultado de la resta es: {result_subtraction}")
+
+# multiplicacion
+result_multiplication = number1 * number2
+# print(f"El resultado de la multiplicacion es: {result_multiplication}")
+
+# division
+result_division = number1 / number2
+# print(f"El resultado de la division es: {result_division}") 
+
+# modulo o residuo
+result_modulo = number1 % number2
+# print(f"El resultado del modulo es: {result_modulo}")
+
+# Operadores de comparacion
+# mayor que >
+# menor que <
+# igual a ==
+# diferente de !=
+# mayor o igual que >=
+# menor o igual que <=
+
+
+# condicional simple
+# age = 17
+# if age >= 18:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+# Condicional anidado
+# age = 16
+# weekday = "Jueves"
+
+# if age >= 18:
+#     if weekday == "Viernes":
+#         print("Eres mayor de edad y es viernes")
+#     else:
+#         print("Eres mayor de edad pero no es viernes")
+# else:
+#     print("Eres menor de edad")
+
+# condicionales en cadena
+# age = 19
+# weekday = "Lunes"
+
+# if age >= 18:
+#     if weekday == "Viernes":
+#         print("Eres mayor de edad y es viernes")
+#     elif weekday == "Sábado":
+#         print("Eres mayor de edad y es sábado")
+#     elif weekday == "Domingo":
+#         print("Eres mayor de edad y es domingo")
+#     else:
+#         print("Eres mayor de edad pero no es fin de semana")
+# else:
+#     print("Eres menor de edad")
+
+# Operadores logicos
+# age = 19
+# weekday = "Lunes"
+
+# if (age >= 18) and ((weekday == "Viernes") or (weekday == "Sábado")):
+#         print("Eres mayor de edad y es viernes o sábado")
+# else:
+#     print("Eres menor de edad")
+
+# estructuras de datos
+# cadena de texto
+greeting = "Hola, ¿cómo estás?"
+number1 = 10
+number_float = 3.14
+is_raining = False  
+
+# lista
+fruits = ["manzana", "banana", "naranja", 10, 3.14, True]
+names = [
+    "Juan",
+    "María",
+    "Pedro"
+    ]
+
+# names.pop()
+
+# add a new name to the list
+# names.append("Ana")
+# print(names[-1])
+
+# tupla
+coordinates = (10.0, 20.0, 30.0)
+
+# print(coordinates[0])
+
+names[0] = "Carlos"
+# print(names[0])
+
+# coordinates[0] = 15.0
+# print(coordinates[0])
+
+# diccionario
+person = {
+    "name": "Juan",
+    "age": 30,
+    "height": 1.75,
+    "is_student": True,
+    "grades": [8, 9, 10]
+}
+
+print(person["name"])
